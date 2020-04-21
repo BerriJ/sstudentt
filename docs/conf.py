1a8fd9ba793266b17afcc3631c98dff1e1bf2618
Jonathan Berrisch <Jonathan@Berrisch.biz>
2020-04-21 23:41:01 +0200

    Use read the docs sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_typo3_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -157,6 +157,4 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-
-extensions.append('sphinx_typo3_theme')
 
