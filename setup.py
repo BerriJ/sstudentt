858b285e608594c14b207de3a9a0db8efb7dc6e7
Jonathan Berrisch <jonathan@berrisch.biz>
2021-06-05 22:29:58 +0200

    Update development status

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email='jonathan@berrisch.biz',
     python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
