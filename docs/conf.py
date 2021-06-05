660b122e589024e0fc677d2d5e9b140d24dcd647
Jonathan Berrisch <jonathan@berrisch.biz>
2021-06-05 22:19:07 +0200

    Switch to rtd sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 import sstudentt
@@ -31,7 +32,7 @@ import sstudentt
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -83,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 
 
 # Theme options are theme-specific and customize the look and feel of a
@@ -161,4 +162,3 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-
